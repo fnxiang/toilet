9299e0b94a9afe07c50e1bb930d6d6111fdfd87c
Amose-Yao <12301111@bjtu.edu.cn>
2022-03-03 18:06:33 +0800

    button infor update

--- src/main/java/cn/edu/bjtu/toilet/controller/IndexController.java
+++ src/main/java/cn/edu/bjtu/toilet/controller/IndexController.java
@@ -8,20 +8,32 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 public class IndexController {
 
-    @RequestMapping(value = "/")
+    @RequestMapping(value = "/login")
     public String index(){
         return "/base/login_final";
     }
 
-    @RequestMapping(value = "/login")
+    @RequestMapping(value = "/login_final")
     public String login(HttpServletRequest request){
         String content = request.getParameter("content");
 
         return "/base/login_final";
     }
 
-    @RequestMapping(value = "/register")
-    public String register(){
+    @RequestMapping(value = "/register_company")
+    public String register_company(){
         return "/base/register_final";
+    }
+
+    @RequestMapping(value = "/register_professor")
+    public String register_professor(){
+        return "base/register_professor";
+    }
+
+    @RequestMapping(value = "/toBasePage")
+    public String toPage(HttpServletRequest request){
+        String url = request.getParameter("url");
+        url = "/base/" + url;
+        return url;
     }
 }
